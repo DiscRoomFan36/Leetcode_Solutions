980cd93b28b0d7378c1594b98ba9bf04cc6b618b
DiscRoomFan36 <fletcherhogarth@gmail.com>
2026-03-04 12:33:43 +1100

    717. 1-bit and 2-bit Characters

---
+++ days/0717_1_bit_and_2_bit_Characters/main.go
@@ -0,0 +1,10 @@
+package main
+
+func isOneBitCharacter(bits []int) bool {
+    for len(bits) > 1 {
+        // advance 1 or 2 if num == 1
+        if bits[0] == 1 { bits = bits[2:];
+        } else          { bits = bits[1:]; }
+    }
+    return len(bits) == 1;
+}
