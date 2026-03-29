fa199e0f91ba9da23c082f790df6b7deb5a310f5
DiscRoomFan36 <fletcherhogarth@gmail.com>
2026-03-29 17:45:45 +1100

    cleanup

--- days/0145_Binary_Tree_Postorder_Traversal/main.go
+++ days/0145_Binary_Tree_Postorder_Traversal/main.go
@@ -1,14 +1,11 @@
-
 package main
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
 func postorderTraversal(root *TreeNode) []int {
     if root == nil { return []int{} }
 
