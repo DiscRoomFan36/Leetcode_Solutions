3ecee6773cac9d4631919d664c6ca111abaec89d
DiscRoomFan36 <fletcherhogarth@gmail.com>
2026-05-22 08:57:10 +1000

    1646. Get Maximum in Generated Array

---
+++ days/1646_Get_Maximum_in_Generated_Array/main.go
@@ -0,0 +1,24 @@
+package main
+
+func getMaximumGenerated(n int) int {
+    if n == 0 { return 0; }
+
+    nums := make([]int, n+1);
+
+    nums[0] = 0;
+    nums[1] = 1;
+
+    for i := 2; i <= n; i++ {
+        if i % 2 == 0 {
+            nums[i] = nums[i/2];
+        } else {
+            nums[i] = nums[i / 2] + nums[i / 2 + 1];
+        }
+    }
+
+    maximum := 1;
+    for _, num := range nums {
+        maximum = max(maximum, num); 
+    }
+    return maximum;
+}
