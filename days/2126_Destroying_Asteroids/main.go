7dcb64cec5ccb1591c0d9ac820ef8179e6272375
DiscRoomFan36 <fletcherhogarth@gmail.com>
2026-05-31 11:43:39 +1000

    2126. Destroying Asteroids

---
+++ days/2126_Destroying_Asteroids/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "slices"
+
+func asteroidsDestroyed(mass int, asteroids []int) bool {
+    slices.Sort(asteroids);
+
+    for _, asteroid := range asteroids {
+        if mass < asteroid { return false; }
+        mass += asteroid;
+    }
+    return true;
+}
